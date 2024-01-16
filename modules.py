75ba011fbc2b69ea42f0f4750a5eb41f726d4ed7
Robert Andersson <randersson85@gmail.com>
2024-01-16 14:30:01 +0100

    skapar branch dev

---
+++ modules.py
@@ -0,0 +1,7 @@
+# funktion för att skriva ut tal som är jämnt delbara med de två värden som anges av användaren.
+def task1():
+    print("Kvar att implementera")
+
+
+def randomnumber():
+    print("Kvar att implementera")
---
+++ program.py
@@ -0,0 +1,3 @@
+import modules
+
+modules.task1()
