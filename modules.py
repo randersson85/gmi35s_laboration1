c46de24058cdf6950f87ba19690298a06e5068dc
Robert Andersson <randersson85@gmail.com>
2024-01-16 17:05:11 +0100

    klar med grundläggande uppgifter i labben

--- modules.py
+++ modules.py
@@ -1,23 +1,64 @@
+import random
 # funktion för att skriva ut tal som är jämnt delbara med de två värden som anges av användaren.
-def task1(division1, division2):
-    #Tar strängvärdet från input och konverterar detta till int istället för string.
-    division1 = int(division1)
-    division2 = int(division2)
-    #Skapar list som innehåller samtliga nummer.
+def division():
+        # https://stackoverflow.com/questions/3289601/null-object-in-python
+        # Tar strängvärdet från input och konverterar detta till int istället för string.
+        # Skapar list som innehåller samtliga nummer.
+    division1 = None
+    division2 = None
     list_of_numbers = []
-    #Skapar en forloop som loopar igenom samtliga heltal från 1 till 1600.
+    while division1 is None:
+        try:
+            division1 = int(input("Ange det första talet: "))
+        except ValueError:
+            print("Det går endast att ange heltal, vänligen försök igen")
+    while division2 is None:
+        try:
+            division2 = int(input("Ange det andra talet: "))
+        except ValueError:
+            print("Det går endast att ange heltal, vänligen försök igen")
+
+            # Skapar en forloop som loopar igenom samtliga heltal från 1 till 1600.
     for x in range(1, 1601):
-        #Använder modulo för att hitta tal som inte lämnar någon heltalsrest.
+    # Använder modulo för att hitta tal som inte lämnar någon heltalsrest.
         if x % division1 == 0:
-            #Lägger till jämnt delbara tal i list_of_numbers med append.
+        # Lägger till jämnt delbara tal i list_of_numbers med append.
             list_of_numbers.append(x)
         elif x % division2 == 0:
-            list_of_numbers.append(x)
+         list_of_numbers.append(x)
     print(list_of_numbers)
 # https://www.w3schools.com/python/gloss_python_for_range.asp
 # https://stackoverflow.com/questions/64030305/is-there-a-way-to-accept-only-the-integer-values-after-dividing-a-list
 
+
+
+
+
 #Funktion för att låta användaren gissa på ett tal mellan 1 - 60.
 
-#   def randomnumber():
-#   print("Kvar att implementera")
+
+def randomnumber():
+    # Tilldelar variabeln random_number ett slumpat tal mellan 1 - 60
+    # https://www.w3schools.com/python/ref_random_randint.asp
+    random_number = random.randint(1, 60)
+    guess = 0
+    while guess != random_number:
+        # https://stackoverflow.com/questions/8075877/converting-string-to-int-using-try-except-in-python
+        try:
+            guess = int(input("Gissa på ett tal mellan 1 - 60: "))
+            if guess > 60 or guess < 0 or guess == 0:
+                print("Ogiltig gissning, talet är mellan 1 - 60")
+
+            elif guess > random_number:
+                print("Talet är lägre än din gissning")
+
+            elif guess < random_number:
+                print("Talet är högre än din gissning")
+
+            elif guess == random_number:
+                print("Rätt gissat")
+
+        except ValueError:
+            print("Det går endast att ange heltal, vänligen försök igen")
+
+
--- program.py
+++ program.py
@@ -1,5 +1,29 @@
 import modules
-print("Uppgift 1: lista heltal utifrån användarinmatning")
+"""print("Uppgift 1: lista heltal utifrån användarinmatning")
 division1 = input("ange värde 1: ")
 division2 = input("Ange Värde 2: ")
-modules.task1(division1,division2)
+modules.division(division1, division2)"""
+
+#modules.randomnumber()
+
+# https://www.geeksforgeeks.org/switch-case-in-python-replacement/
+
+menu_selector = 0
+while menu_selector != 3:
+    print("----------------------------------------------------------------------\n"
+          "1. Skapa lista med tal som är jämnt delbara med två av dina val\n"
+          "2. Gissa på ett tal mellan 1 - 60\n"
+          "3. Avsluta programmet\n"
+          "----------------------------------------------------------------------")
+
+    try:
+        menu_selector = int(input(""))
+        if menu_selector == 1:
+            modules.division()
+        elif menu_selector == 2:
+            modules.randomnumber()
+        elif menu_selector == 3:
+            print("Avslutar programmet")
+
+    except ValueError:
+        print("Ogiltigt menyval, val måste vara heltal mellan 1 - 3")
