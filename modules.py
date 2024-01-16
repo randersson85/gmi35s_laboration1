ecbfa12de2e5b4175a5f3c7702d45d7074e24667
Robert Andersson <randersson85@gmail.com>
2024-01-16 17:48:18 +0100

    lagt till medelvärde och formaterat detta

--- modules.py
+++ modules.py
@@ -1,20 +1,25 @@
 import random
+import statistics
 # funktion för att skriva ut tal som är jämnt delbara med de två värden som anges av användaren.
 def division():
         # https://stackoverflow.com/questions/3289601/null-object-in-python
         # Tar strängvärdet från input och konverterar detta till int istället för string.
         # Skapar list som innehåller samtliga nummer.
-    division1 = None
-    division2 = None
+    division1 = 0
+    division2 = 0
     list_of_numbers = []
-    while division1 is None:
+    while division1 is 0:
         try:
             division1 = int(input("Ange det första talet: "))
+            if division1 <= 0:
+                print("Talet kan måste vara mer än 0")
         except ValueError:
             print("Det går endast att ange heltal, vänligen försök igen")
-    while division2 is None:
+    while division2 is 0:
         try:
             division2 = int(input("Ange det andra talet: "))
+            if division1 <= 0:
+                print("Talet kan måste vara mer än 0")
         except ValueError:
             print("Det går endast att ange heltal, vänligen försök igen")
 
@@ -25,8 +30,11 @@ def division():
         # Lägger till jämnt delbara tal i list_of_numbers med append.
             list_of_numbers.append(x)
         elif x % division2 == 0:
-         list_of_numbers.append(x)
+            list_of_numbers.append(x)
     print(list_of_numbers)
+    # https://www.geeksforgeeks.org/find-median-of-list-in-python/
+    mean = "{:.2f}".format(statistics.mean(list_of_numbers))
+    print ("Medelvärdet på listan är " + mean)
 # https://www.w3schools.com/python/gloss_python_for_range.asp
 # https://stackoverflow.com/questions/64030305/is-there-a-way-to-accept-only-the-integer-values-after-dividing-a-list
 
